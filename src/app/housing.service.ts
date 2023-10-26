7e2a862307730b1f8f02f8a67e83757f5dfe8f3b
Bonny ARDITO <ardito.b@gmail.com>
2023-10-26 17:40:32 +0200

    ajout recherche

--- src/app/housing.service.ts
+++ src/app/housing.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from "@angular/core";
 import { HousingLocation } from "./housinglocation";
+
 @Injectable({
   providedIn: "root",
 })
@@ -116,11 +117,6 @@ export class HousingService {
   getHousingLocationById(id: number): HousingLocation | undefined {
     return this.housingLocationList.find(
       (housingLocation) => housingLocation.id === id
-    );
-  }
-  submitApplication(firstName: string, lastName: string, email: string) {
-    console.log(
-      `Homes application received: firstName: ${firstName}, lastName: ${lastName}, email: ${email}.`
     );
   }
 }
